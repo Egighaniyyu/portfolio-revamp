95472f2299bf215aa42f69697dcac7193001569d
Egighaniyyu <60068108+Egighaniyyu@users.noreply.github.com>
2023-12-01 12:26:41 +0700

    chore(project) : add mouseover for thumbnail project

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,13 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import Mainlayouts from "../layouts/Mainlayouts";
 
-import Portfolio1 from "../assets/images/portfolio1.png";
+const Project = () => {
+  const [portfolioImage, setPortfolioImage] = useState(
+    "/public/images/portfolio1.png"
+  );
+  const [lastHoveredImage, setLastHoveredImage] = useState("");
 
-export default function Project() {
+  const handleProjectLinkHover = (project) => {
+    setPortfolioImage(project.image);
+    setLastHoveredImage(project.image);
+  };
+
+  const handleProjectLinkLeave = () => {
+    setPortfolioImage(lastHoveredImage);
+  };
+
+  const projects = [
+    {
+      title: "IDPods E-Commerce",
+      link: "https://idpods.co.id/",
+      image: "/public/images/portfolio1.png",
+    },
+    {
+      title: "Finance Mobile Apps",
+      link: "https://dribbble.com/shots/22088961-CashWave-Finance-Mobile-APP",
+      image: "/public/images/portfolio2.png",
+    },
+    {
+      title: "Smart Home Mobile Apps",
+      link: "https://dribbble.com/shots/22083501-SmartLiving-Smart-Home-Mobile-App",
+      image: "/public/images/portfolio3.png",
+    },
+    {
+      title: "Weather Mobile Apps",
+      link: "https://dribbble.com/shots/19903136-Weather-App",
+      image: "/public/images/portfolio4.png",
+    },
+  ];
+
   return (
     <>
       <Mainlayouts>
-        <div className="container flex justify-between items-center w-full">
+        <div className="container flex justify-between items-center w-full mb-10">
           <div className="w-5/12">
             <div className="flex flex-col items-start gap-16">
               <div className="flex flex-col items-start gap-5">
@@ -15,31 +50,25 @@ export default function Project() {
                 <div className="subtitle-page">CREATIVITY IN EVERY CLICK</div>
               </div>
               <div className="flex flex-col items-start gap-6 w-full h-60 overflow-y-scroll">
-                <a href="#" className="project-menu">
-                  IDPods E-Commerce
-                </a>
-                <a href="#" className="project-menu">
-                  Kitabisa Revamp Apps
-                </a>
-                <a href="#" className="project-menu">
-                  E-Learning Mobile Apps
-                </a>
-                <a href="#" className="project-menu">
-                  Finance Mobile Apps
-                </a>
-                <a href="#" className="project-menu">
-                  Smart Home Mobile Apps
-                </a>
-                <a href="#" className="project-menu">
-                  Weather Mobile Apps
-                </a>
+                {projects.map((project) => (
+                  <a
+                    key={project.title}
+                    href={project.link}
+                    className="project-menu"
+                    onMouseEnter={() => handleProjectLinkHover(project)}
+                    onMouseLeave={handleProjectLinkLeave}
+                    target="_blank"
+                  >
+                    {project.title}
+                  </a>
+                ))}
               </div>
             </div>
           </div>
           <div className="w-7/12">
             <img
-              src={Portfolio1}
-              alt="portfolio1"
+              src={portfolioImage}
+              alt="portfolio"
               className="w-10/12 h-auto ml-auto"
             />
           </div>
@@ -47,4 +76,6 @@ export default function Project() {
       </Mainlayouts>
     </>
   );
-}
+};
+
+export default Project;
