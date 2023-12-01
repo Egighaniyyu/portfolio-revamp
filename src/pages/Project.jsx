511b6ef0f66992ed975e77f63d9a175729737809
Egighaniyyu <60068108+Egighaniyyu@users.noreply.github.com>
2023-12-01 12:46:42 +0700

    fix(project) : fix thumbnail project

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -20,22 +20,22 @@ const Project = () => {
     {
       title: "IDPods E-Commerce",
       link: "https://idpods.co.id/",
-      image: "/public/images/portfolio1.png",
+      image: "/images/portfolio1.png",
     },
     {
       title: "Finance Mobile Apps",
       link: "https://dribbble.com/shots/22088961-CashWave-Finance-Mobile-APP",
-      image: "/public/images/portfolio2.png",
+      image: "/images/portfolio2.png",
     },
     {
       title: "Smart Home Mobile Apps",
       link: "https://dribbble.com/shots/22083501-SmartLiving-Smart-Home-Mobile-App",
-      image: "/public/images/portfolio3.png",
+      image: "/images/portfolio3.png",
     },
     {
       title: "Weather Mobile Apps",
       link: "https://dribbble.com/shots/19903136-Weather-App",
-      image: "/public/images/portfolio4.png",
+      image: "/images/portfolio4.png",
     },
   ];
 
