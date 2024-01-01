67907ed71b398135c1931aa338f13a63a43e7796
Egighaniyyu <60068108+Egighaniyyu@users.noreply.github.com>
2024-01-01 10:11:28 +0700

    chore(project) : add tag project

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -24,36 +24,98 @@ const Project = () => {
       title: "Case Study : Kitabisa Revamp",
       link: "https://medium.com/@egighaniyyu/ui-ux-case-study-menu-kabar-terbaru-kitabisa-com-b07d1c408c29",
       image: "/images/portfolio5.png",
+      tag: [
+        {
+          title: "Case Study",
+          class: "case-study",
+        },
+        {
+          title: "UI Design",
+          class: "ui-design",
+        },
+        {
+          title: "UX Design",
+          class: "ux-design",
+        },
+      ],
     },
     {
       title: "Case Study : PWMP Link Redesign",
       link: "https://egighaniyyu.notion.site/UI-UX-Case-Study-PWMP-Link-App-Revamp-17a8348f2a5945d3bede4fc6871f2a18?pvs=4",
       image: "/images/portfolio6.png",
+      tag: [
+        {
+          title: "Case Study",
+          class: "case-study",
+        },
+        {
+          title: "UI Design",
+          class: "ui-design",
+        },
+        {
+          title: "UX Design",
+          class: "ux-design",
+        },
+      ],
     },
     {
       title: "Project : IDPods E-Commerce",
       link: "https://idpods.co.id/",
       image: "/images/portfolio1.png",
+      tag: [
+        {
+          title: "UI Design",
+          class: "ui-design",
+        },
+        {
+          title: "UX Design",
+          class: "ux-design",
+        },
+      ],
     },
     {
       title: "UI Design : SIM-RS | Medical Dashboard",
       link: "https://dribbble.com/shots/22134409-SIM-RS-Medical-Dashboard",
       image: "/images/portfolio7.png",
+      tag: [
+        {
+          title: "UI Design",
+          class: "ui-design",
+        },
+      ],
     },
     {
       title: "UI Design : Finance Mobile Apps",
       link: "https://dribbble.com/shots/22088961-CashWave-Finance-Mobile-APP",
       image: "/images/portfolio2.png",
+      tag: [
+        {
+          title: "UI Design",
+          class: "ui-design",
+        },
+      ],
     },
     {
       title: "UI Design : Smart Home Mobile Apps",
       link: "https://dribbble.com/shots/22083501-SmartLiving-Smart-Home-Mobile-App",
       image: "/images/portfolio3.png",
+      tag: [
+        {
+          title: "UI Design",
+          class: "ui-design",
+        },
+      ],
     },
     {
       title: "UI Design : Weather Mobile Apps",
       link: "https://dribbble.com/shots/19903136-Weather-App",
       image: "/images/portfolio4.png",
+      tag: [
+        {
+          title: "UI Design",
+          class: "ui-design",
+        },
+      ],
     },
   ];
 
@@ -91,11 +153,31 @@ const Project = () => {
             </div>
           </div>
           <div className="hidden sm:block w-12/12 sm:w-7/12">
-            <img
-              src={portfolioImage}
-              alt="portfolio"
-              className="w-12/12 pt-4 sm:pt-0 sm:w-10/12 h-auto ml-auto"
-            />
+            <div className="flex flex-col gap-5 pl-20">
+              <img
+                src={portfolioImage}
+                alt="portfolio"
+                className="w-12/12 pt-4 sm:pt-0 sm:w-10/12 h-auto"
+              />
+
+              {projects.map((projectItem) => {
+                if (projectItem.image === portfolioImage) {
+                  return (
+                    <div
+                      key={projectItem.title}
+                      className="flex flex-row gap-2"
+                    >
+                      {projectItem.tag.map((tag) => (
+                        <div className={`tag ${tag.class}`} key={tag.title}>
+                          {tag.title}
+                        </div>
+                      ))}
+                    </div>
+                  );
+                }
+                return null;
+              })}
+            </div>
           </div>
         </div>
 
